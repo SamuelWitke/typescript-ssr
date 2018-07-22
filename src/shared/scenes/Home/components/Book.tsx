6454e6fc1c06eae4f23f7e5145ea4af7b8868551
SamuelWitke <SamuelWitke@gmail.com>
2018-07-22 11:16:02 -0400

    Working State

---
+++ src/shared/scenes/Home/components/Book.tsx
@@ -0,0 +1,63 @@
+import {requestBookAndSimilars} from '../../../actions/book';
+import {addToCart} from '../../../actions/cart';
+import * as React from 'react';
+import { Segment, Label, Button } from 'semantic-ui-react'
+//import { StoreState } from '../../types/';
+import { connect, Dispatch } from 'react-redux';
+
+
+
+class BookDetails extends React.Component<any,any,any>{
+	constructor(props : any) {
+		super(props);
+	}
+
+	componentDidMount() {
+		this.props.requestBookAndSimilars(this.props.id);
+	}
+
+	handleAddToCart = () => {
+		this.props.addToCart(this.props.id);
+	}
+
+	render() {
+		const { id, series, title, author, image, tags, similarBooks } = this.props;
+		return (
+			<Segment.Group>
+				<Segment size='big'>{series}</Segment>
+				<Segment>
+					<img src={image} alt={name} className='cover' />
+					<h1>ID: {id} - {title}</h1>
+					<h2>By {author}</h2>
+					{ tags && tags.map((tag :any) => <Label key={tag} color='yellow'>{tag}</Label>) }
+			<Button content='Buy' icon='shop' onClick={this.handleAddToCart} />
+			<div className='similar'> 
+				<h3>You might also like:</h3>
+				{ similarBooks && similarBooks.map( (similar :any) => (
+					<img key={similar.id} src={similar.image} alt={similar.name} className='similar_cover' />
+					)) }
+			</div>
+
+		</Segment>
+	</Segment.Group>
+	)
+	}
+}
+
+export function mapStateToProps({ books}: any) {
+	const { selectedBook, similar } = books;
+	return {
+		...selectedBook,
+		series: selectedBook.series || selectedBook.title, 
+		similarBooks: similar
+	}
+}
+
+export function mapDispatchToProps(dispatch: Dispatch<any>) {
+	return {
+		requestBookAndSimilars: (id : number) => dispatch(requestBookAndSimilars(id)),
+		addToCart: (id: number) => dispatch(addToCart(id)),
+	};
+}
+
+export default connect<any,any,any>(mapStateToProps, mapDispatchToProps)(BookDetails);
--- src/shared/scenes/Home/index.tsx
+++ src/shared/scenes/Home/index.tsx
@@ -1,23 +1,50 @@
-import Hello from './components/Home';
-import {requestBookAndSimilars} from '../../actions/book';
-import {addToCart} from '../../actions/cart';
-//import { StoreState } from '../../types/';
-import { connect, Dispatch } from 'react-redux';
+import * as React from 'react';
+import { Dropdown, Segment } from 'semantic-ui-react'
+import Book from './components/Book';
+import './Home.css';
 
-export function mapStateToProps({ books}: any) {
-	const { selectedBook, similar } = books;
-	return {
-		...selectedBook,
-		series: selectedBook.series || selectedBook.title, 
-		similarBooks: similar
+type State = {
+	selectedId: number | null,
+}
+
+class Home extends React.Component<any,State> {
+	constructor(props : any) {
+		super(props);
+		this.state = {
+			selectedId: null 
+		};
+	}
+
+	render() {
+		console.log(this.state);	
+		return (
+			<div>
+				{ this.state.selectedId !== null ?
+				<Book id={this.state.selectedId} />
+				: 
+					<Segment>
+						<Dropdown
+							placeholder='Select a Book'
+							options={[
+								{ text: 'Harry Potter', value: 1 },
+								{ text: 'Lord of the Rings', value: 2 },
+								{ text: 'Game of Thrones', value: 3 },
+								{ text: 'Sherlock Holmes', value: 4 },
+								{ text: 'Murder in the Orient Express', value: 5 },
+								{ text: 'Neuromancer', value: 6 },
+								{ text: 'Ready Player One', value: 7 },
+							]}
+							onChange={(e, selected) => this.setState({selectedId: selected.value} as State)}
+
+							fluid
+							search
+							selection
+						/>
+					</Segment>
+					}
+			</div>
+			);
 	}
 }
 
-export function mapDispatchToProps(dispatch: Dispatch<any>) {
-	return {
-		requestBookAndSimilars: (id : number) => dispatch(requestBookAndSimilars(id)),
-		addToCart: (id: number) => dispatch(addToCart(id)),
-	};
-}
-
-export default connect<any,any>(mapStateToProps, mapDispatchToProps)(Hello);
+export default Home;
